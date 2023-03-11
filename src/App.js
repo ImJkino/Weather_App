3afaa67b071595dea21c8ca1f2b1e0ae62ec9c8d
ImJkino <aquino.joseph94@gmail.com>
2023-03-11 09:21:44 -0800

    just need to commit some stuff

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+import "./App.css";
+import Inputs from "./Components/Inputs";
+import TopButtons from "./Components/TopButtons";
+// import UilReact from '@iconscout/react-unicons/icons/uil-react';
 
 function App() {
   return (
     <div className="App">
-      <h1 className='text-2xl font-bold text-red-500'>Hello</h1>
+      <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+        <TopButtons />
+        <Inputs />
+      </div>
     </div>
   );
 }
