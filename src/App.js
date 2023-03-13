dc763160e13d9aa778e6a8b9df898f6453700a7e
ImJkino <aquino.joseph94@gmail.com>
2023-03-13 09:23:55 -0700

    adding more

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
 import "./App.css";
+import { useEffect, useState } from "react";
+import Forecast from "./Components/Forecast";
 import Inputs from "./Components/Inputs";
+import TempAndDetail from "./Components/TempAndDetail";
+import TimeAndLocation from "./Components/TimeAndLocation";
 import TopButtons from "./Components/TopButtons";
+import getFormattedWeatherData from "./Services/weatherService";
+// import getWeatherData from "./Services/weatherService";
 // import UilReact from '@iconscout/react-unicons/icons/uil-react';
 
 function App() {
+  const [query, setQuery] = useState({ q: "london" });
+  const [units, setUnits] = useState("metric");
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    const fetchWeather = async () => {
+      await getFormattedWeatherData({ ...query, units }).then((data) => {
+        setWeather(data);
+        console.log("data------>", data);
+      });
+    };
+    fetchWeather();
+  }, [query, units]);
+
   return (
-    <div className="App">
-      <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
-        <TopButtons />
-        <Inputs />
-      </div>
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+      <TopButtons />
+      <Inputs />
+
+      {weather && (
+        <div>
+          <TimeAndLocation weather={weather} />
+          <TempAndDetail weather={weather} />
+
+          {/* Need to subscribe for hourly forecast */}
+          {/* <Forecast title="Hourly Forecast" /> */}
+
+          <Forecast title="daily forecast" items={weather.list} />
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/Services/weatherService.js
@@ -0,0 +1,113 @@
+import { DateTime } from "luxon";
+const API_KEY = "66c6494b8365dc9919bebdc58ddf9312";
+
+const BASE_URL = "https://api.openweathermap.org/data/2.5";
+
+// https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+
+const getWeatherData = (infoType, searchParams) => {
+  const url = new URL(BASE_URL + "/" + infoType);
+  url.search = new URLSearchParams({ ...searchParams, appid: API_KEY });
+  return fetch(url).then((response) => response.json());
+};
+
+const formatCurrentWeather = (data) => {
+  const {
+    coord: { lat, lon },
+    main: { temp, feels_like, temp_min, temp_max, humidity },
+    name,
+    dt,
+    sys: { country, sunrise, sunset },
+    weather,
+    wind: { speed },
+    timezone,
+  } = data;
+
+  const { main: details, icon } = weather[0];
+
+  return {
+    lat,
+    lon,
+    temp,
+    feels_like,
+    temp_min,
+    temp_max,
+    humidity,
+    name,
+    dt,
+    country,
+    sunrise,
+    sunset,
+    details,
+    icon,
+    speed,
+    timezone,
+  };
+};
+
+const WEEK_DAYS = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+const dayInWeek = new Date().getDay();
+const forecastDays = WEEK_DAYS.slice(dayInWeek, WEEK_DAYS.length).concat(
+  WEEK_DAYS.slice(0, dayInWeek)
+);
+//////////////////// Need to subscribe to get hourly forecast//////////////////////////
+const formatForecastWeather = (data) => {
+  let { list } = data;
+  list = list.slice(1, 6).map((d, idx) => {
+    return {
+      index: idx,
+      title: forecastDays[idx],
+      temp: d.main.temp,
+      icon: d.weather[0].icon,
+    };
+  });
+  //   hourly = hourly.slice(1, 6).map((d) => {
+  //     return {
+  //       title: formatToLocalTime(d.dt, timezone, "ccc"),
+  //       temp: d.temp,
+  //       icon: d.weather[0].icon,
+  //     };
+  //   });
+  //   console.log("timezone------>", timezone);
+  return { list };
+};
+
+const getFormattedWeatherData = async (searchParams) => {
+  const formattedCurrentWeather = await getWeatherData(
+    "weather",
+    searchParams
+  ).then(formatCurrentWeather);
+
+  const { lat, lon } = formattedCurrentWeather;
+
+  console.log("formattedCurrentWeather=-------->", formattedCurrentWeather);
+
+  const formattedForecastWeather = await getWeatherData("forecast", {
+    lat,
+    lon,
+    units: searchParams.units,
+  }).then(formatForecastWeather);
+
+  return { ...formattedCurrentWeather, ...formattedForecastWeather };
+};
+
+const formatToLocalTime = (
+  secs,
+  zone,
+  format = "cccc, dd LLL yyyy' | Local Time: 'hh:mm a"
+) => DateTime.fromSeconds(secs).setZone(zone).toFormat(format);
+
+const iconUrlFromCode = (code) =>
+  `http://openweathermap.org/img/wn/${code}@2x.png`;
+
+export default getFormattedWeatherData;
+
+export { iconUrlFromCode, formatToLocalTime };
